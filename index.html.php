f2808e2081ae470f57d7b0317b7d0159a013f7b3
s220016763@mandela.ac.za <109454674+ZalZip53@users.noreply.github.com>
2022-10-06 19:56:40 +0200

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,197 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>My Website</title>
+  <link href='all.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+   <!-- Header -->
+   <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero">
+            <h1><span>S</span>PORO <span>T</span>ech <span>S</span>OLUTIONS</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#services" data-after="Service">Services</a></li>
+            
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+            <li><a href="" data-after="Contact">Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+ 
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1> <span></span></h1>
+        <h1>SporoTechSolutions <span></span></h1>
+        <h1> <span></span></h1>
+        <a href="" type="button" class="cta">Sign Up</a>
+        
+      </div>
+      <section id="logo">
+      <div class=" logo container">
+        <div class="card">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="sporo.jpg" alt="img">
+        </div>
+      </div>
+    </div>
+</div>
+</section>
+
+  </section>
+  <!-- End Hero Section  -->
+
+  <!-- Service Section -->
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Serv<span>i</span>ces</h1>
+        <p><b>IT services refers to the application of business and technical expertise to enable organizations in the creation, management and optimization of or access to information and business processes. The IT services market can be segmented by the type of skills that are employed to deliver the service (design, build, run)</b></p>
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Web Design</h2>
+          <p>Web development refers in general to the tasks associated with developing websites for hosting via intranet or internet. The web development process includes web design, web content development, client-side/server-side scripting and network security configuration, among other tasks.</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Digital Marketing <br/> Social Media Marketing</h2>
+          <p>Social media marketing (SMM) is a form of internet marketing that uses social media apps as a marketing tool.<br/>build a brand;
+          <br/>increase sales;
+          <br/>drive traffic to a website; and
+          <br/>build a community of followers to share and engage with content. </p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>IT Support Services</h2>
+          <p>A technical support/helpdesk monitor and maintaining the computer systems and networks within an organisation in a technical support role.
+
+.The  technical support/helpdesk help with challenges  such as forgotten passwords, viruses or email issues, 
+
+Tasks can include installing and configuring computer systems,</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Software Development Tutorials</h2>
+          <p>Assisting Software Development university undergraduates in Information Systemse <br/> ERD's ,Use Cases,Prototypes,Project Portfolio etc Code Writing <br/> MVC ,PHP,C# ,Database etcOther e.g Version Controls ,Publishing</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Service Section -->
+
+ 
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="sporo2.jpg" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">About <span>Zipho Siyabonga Ntuli</span></h1>
+        <h2><b>The  Director</b></h2>
+        <p><b>A Software Developer. Bringing forth a motivated attitude
+and a variety of powerful skills. Highly organized, and skilled in code writing and
+verbal communication. A strong leader who works well in any circumstances, and express
+positive energy among his group. Knowledgeable in user interface, testing, and
+debugging processes. Proficient in an assortment of technologies, including CSS,ASP.NET, C#, IIS, MVC,PHP and Microsoft SQL Server. Bringing forth a
+proven track record of successfully academic effort</b></p>
+        <a href="cv.pdf" class="cta">Download Resume</a>
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2><b>0676470853</b></h2>
+            <h2><b>0738350486</b></h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2><b>ziphosiyabonga@gmail.com</b></h2>
+            <h2><b>s220016763@mandela.ac.za</b></h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+          <div class="contact-info">
+            <h1>Adress</h1>
+            <h2><b>Annie Cove ,Summerstrand, Port Elizabeth </b></h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>S</span>poro <span>T</span>ech <span>S</span>olutions</h1>
+      </div>
+      <h2>Your Complete Web Solution</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="https://www.facebook.com/ziphosiyabonga.ntuli/"><img src="face.png" /></a>
+        </div>
+        <div class="social-item">
+                  <a href="https://www.instagram.com/zipho_siyabonga_ntuli/">      
+                    <img src="insta.png" alt=""> 
+</a>
+        </div>
+        
+        <div class="social-item">
+          <a href="https://www.linkedin.com/feed/?trk=sem-ga_campid%3D14650114788_asid%3D127961666300_crid%3D601257986830_kw%3Dlinkedin%20login_d%3Dc_tid%3Dkwd-12704335873_n%3Dg_mt%3De_geo%3D1028624_slid%3D"><img src="linked.png" /></a>
+        </div>
+      </div>
+      
+    </div>
+  </section>
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+  <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+</body>
+
+</html>
